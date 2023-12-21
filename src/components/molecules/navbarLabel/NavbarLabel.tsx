7d9d1a2fb7b768cf2c6aae2b5b5445efe5a7d34c
QStieven <qstievenard@gmail.com>
2023-12-21 13:55:39 +0100

    Add hover effect NavbarLabel

--- src/components/molecules/navbarLabel/NavbarLabel.tsx
+++ src/components/molecules/navbarLabel/NavbarLabel.tsx
@@ -12,9 +12,16 @@ type NavbarLabelProps = {
 
 export const NavbarLabel = ({ iconName, children, onClick, isActive, className }: NavbarLabelProps) => {
   return (
-    <div className={clsx('flex items-center cursor-pointer', className)} onClick={onClick}>
-      <Icon className={isActive ? 'text-gray-200' : 'text-gray-500'} name={iconName} />
-      <p className={clsx('ml-2 text-base font-medium', isActive ? ' text-gray-200' : 'text-gray-500')}>{children}</p>
+    <div
+      className={clsx(
+        'flex items-center cursor-pointer transition-all hover:text-gray-200',
+        className,
+        isActive ? 'text-gray-200' : 'text-gray-500'
+      )}
+      onClick={onClick}
+    >
+      <Icon name={iconName} />
+      <p className="ml-2 text-base font-medium">{children}</p>
     </div>
   )
 }
