c753225430ddbd46c8fd12a99fcc25df3331fffa
QStieven <qstievenard@gmail.com>
2023-12-24 17:46:21 +0100

    add register + login connection

---
+++ src/services/applications/application.ts
@@ -0,0 +1,12 @@
+// import { api } from '../api'
+
+// type GetAllApllicationsUserProps = {
+//   userId: number
+//   token: string
+// }
+
+// export const getAllApplicationsUser = async ({ userId, token }: GetAllApllicationsUserProps) Promise<ResponseAllApplication> => {
+//   return api.get('/applications')
+// }
+
+export const tkt = 'coucou'
--- src/views/login/Login.container.tsx
+++ src/views/login/Login.container.tsx
@@ -1,10 +1,24 @@
 import { ButtonPrimary, ButtonSecondary, Input } from '@/components'
+import { login } from '@/services/auth/auth'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export const LoginContainer = () => {
+  const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const handleOnLogin = async () => {
+    try {
+      const data = await login({ email, password })
+      const token = data.token
+
+      localStorage.setItem('token', token)
+      navigate('/')
+    } catch {
+      alert('Error')
+    }
+  }
 
   return (
     <div className="bg-gray-900 w-full h-full px-17 py-14">
@@ -40,7 +54,7 @@ export const LoginContainer = () => {
                   setPassword(e.target.value)
                 }}
               />
-              <ButtonPrimary isDisabled={!email || !password} className="w-full mt-9">
+              <ButtonPrimary onClick={() => handleOnLogin()} isDisabled={!email || !password} className="w-full mt-9">
                 Se connecter
               </ButtonPrimary>
               <p className="text-gray-300 text-3 flex justify-center mt-26">
