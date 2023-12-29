387f109a23eb40d66bb907aeb91a90a9c2d5c52e
QStieven <qstievenard@gmail.com>
2023-12-29 01:11:08 +0100

    fix indentation

---
+++ src/components/atoms/datePickerInput/DatePickerInput.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx'
+import { Datepicker } from 'flowbite-react'
+import './DatePickerInput.css'
+
+type DatePickerInputProps = {
+  label?: string
+  className?: string
+  onChange?: any
+}
+
+export const DatePickerInput = ({ label, className, onChange }: DatePickerInputProps) => {
+  return (
+    <div className={clsx(className)}>
+      <p className="text-gray-500 text-3.5 mb-3">{label}</p>
+      <Datepicker datepicker-format="yyyy-mm-dd" onSelectedDateChanged={onChange} className="date-picker-input" />
+    </div>
+  )
+}
---
+++ src/components/organisms/modal/Modal.tsx
@@ -0,0 +1,41 @@
+import { Icon } from '@/components/atoms/icons/Icon'
+import clsx from 'clsx'
+import { createPortal } from 'react-dom'
+
+type NewApplicationModalProps = {
+  show: boolean
+  onClose: () => void
+  children: React.ReactNode
+  title: string
+}
+
+export const Modal = ({ show, onClose, children, title }: NewApplicationModalProps) => {
+  const rootPortal = document.getElementById('modal-root')
+  if (!rootPortal) return <></>
+  return createPortal(
+    <div
+      className={clsx(
+        show ? 'fixed' : 'hidden',
+        'top-0 left-0 w-full h-full absolute z-first bg-gray-900/50 flex ',
+        'laptop:items-center laptop:justify-center'
+      )}
+    >
+      <div
+        className={clsx(
+          'w-full p-6 bg-gray-700 rounded-6  max-h-[75vh] fixed bottom-0',
+          'laptop:w-[666px] laptop:block laptop:bottom-auto laptop:h-fit overflow-y-auto'
+        )}
+      >
+        <header className="flex justify-between">
+          <div className="w-4"></div>
+          <p className="text-gray-100 text-3.5 font-medium">{title}</p>
+          <div onClick={onClose} className="w-6 h-6">
+            <Icon name="close" className="text-gray-100 cursor-pointer" />
+          </div>
+        </header>
+        {children}
+      </div>
+    </div>,
+    rootPortal
+  )
+}
