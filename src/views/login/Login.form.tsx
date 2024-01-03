c924c3be2d1db55bdfaad73181d359536c24ad17
QStieven <qstievenard@gmail.com>
2024-01-03 22:12:17 +0100

    add LoaderComponent

--- src/views/login/Login.form.tsx
+++ src/views/login/Login.form.tsx
@@ -2,6 +2,8 @@ import { FormikErrors, useFormik } from 'formik'
 import { ButtonPrimary, Input } from '@/components'
 import { login } from '@/services/auth/auth'
 import { alert } from '@/components/molecules/toast/toast.helper'
+import { useState } from 'react'
+import { LoaderComponent } from '@/components/atoms/loaderComponent/LoaderComponent'
 
 type FormValues = {
   email: string
@@ -25,6 +27,7 @@ const validate = (values: FormValues) => {
 }
 
 export const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const formik = useFormik({
     initialValues: {
       email: '',
@@ -38,41 +41,49 @@ export const LoginForm = () => {
   })
 
   const handleOnLogin = async (credentials: FormValues) => {
+    setIsLoading(true)
     try {
       const data = await login(credentials)
       const token = data.token
 
       localStorage.setItem('token', token)
       window.location.href = '/'
+      setIsLoading(false)
     } catch (error) {
       alert({ type: 'error', message: 'Adresse email ou mot de passe incorrect' })
     }
   }
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <Input
-        errorMessage={formik.errors.email}
-        type="text"
-        name="email"
-        value={formik.values.email}
-        placeholder="Votre email"
-        iconName="mail"
-        onChange={formik.handleChange}
-      />
-      <Input
-        errorMessage={formik.errors.password}
-        className="mt-6"
-        type="password"
-        name="password"
-        value={formik.values.password}
-        placeholder="Votre mot de passe"
-        iconName="lock"
-        onChange={formik.handleChange}
-      />
-      <ButtonPrimary type="submit" className="w-full mt-9">
-        Se connecter
-      </ButtonPrimary>
-    </form>
+    <>
+      {isLoading ? (
+        <LoaderComponent />
+      ) : (
+        <form onSubmit={formik.handleSubmit}>
+          <Input
+            errorMessage={formik.errors.email}
+            type="text"
+            name="email"
+            value={formik.values.email}
+            placeholder="Votre email"
+            iconName="mail"
+            onChange={formik.handleChange}
+          />
+          <Input
+            errorMessage={formik.errors.password}
+            className="mt-6"
+            type="password"
+            name="password"
+            value={formik.values.password}
+            placeholder="Votre mot de passe"
+            iconName="lock"
+            onChange={formik.handleChange}
+          />
+          <ButtonPrimary type="submit" className="w-full mt-9">
+            Se connecter
+          </ButtonPrimary>
+        </form>
+      )}
+    </>
   )
 }
