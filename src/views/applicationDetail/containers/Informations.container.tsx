a81aad5e4fdb76f7ac940558735e4bae6265805d
QStieven <qstievenard@gmail.com>
2024-01-08 14:55:49 +0100

    add Detail part

--- src/views/applicationDetail/containers/Informations.container.tsx
+++ src/views/applicationDetail/containers/Informations.container.tsx
@@ -5,7 +5,7 @@ export const InformationsContainer = () => {
   const { company, job, jobOfferUrl } = useApplicationDetailsContext()
   return (
     <div className="w-full">
-      <div className="flex items-center justify-between mt-13">
+      <div className="flex justify-between mt-13">
         <div className="w-1/2">
           <p className="text-4 text-gray-50 font-bold mb-6">Général</p>
           <div className="flex items-center">
@@ -14,7 +14,13 @@ export const InformationsContainer = () => {
           </div>
           <Input className="w-full mt-6" iconName="link" label="Lien de l'offre" value={jobOfferUrl} />
         </div>
-        <div className="w-1/2">Détails</div>
+        <div className="w-1/2 ml-30">
+          <p className="text-4 text-gray-50 font-bold mb-6">Détails</p>
+          <div className="flex items-center">
+            <Input className="w-full" label="Localisation" iconName="map-pin" placeholder="Localisation" />
+            <Input className="w-full ml-10" label="Type de contrat" iconName="document-remove" placeholder="CDI" />
+          </div>
+        </div>
       </div>
     </div>
   )
