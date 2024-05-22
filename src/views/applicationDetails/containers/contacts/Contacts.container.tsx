7876b7b942dfa6c5c3fdfe7bd341e62cb09f7a0d
QStieven <qstievenard@gmail.com>
2024-05-22 14:54:06 +0200

    feat(application/contact): creating the General Component

--- src/views/applicationDetails/containers/contacts/Contacts.container.tsx
+++ src/views/applicationDetails/containers/contacts/Contacts.container.tsx
@@ -1,8 +1,7 @@
 import { ButtonSecondary } from '@/components'
 import clsx from 'clsx'
-import { ContactCard } from './components/contactCard/contactCard'
 import { useState } from 'react'
-import { ContactModal } from './components/modals/contactModal'
+import { General } from './components/General/General'
 
 export const ContactsContainer = () => {
   const [showContactModal, setShowContactModal] = useState(false)
@@ -19,26 +18,23 @@ export const ContactsContainer = () => {
       job: 'Head of RH',
       mail: 'brdnicolas.contact@gmail.com',
       phone: '0767939258',
+      favorite: false
+    },
+    {
+      name: 'Quentin Stievenard',
+      job: 'Developper',
+      mail: 'qstievenard.contact@gmail.com',
+      phone: '0610781764',
       favorite: true
+    },
+    {
+      name: 'Quentin Stievenard',
+      job: 'Developper',
+      mail: 'qstievenard.contact@gmail.com',
+      phone: '0610781764',
+      favorite: false
     }
   ]
-
-  const displayFavoriteContacts = () => {
-    const favoriteContacts = contacts.filter((contact) => contact.favorite === true)
-
-    if (favoriteContacts.length === 0) {
-      return
-    }
-
-    return (
-      <div className="flex flex-col">
-        <h2 className={clsx('text-4 text-gray-50 font-bold')}>Favoris</h2>
-        {favoriteContacts.map((contact) => {
-          return <ContactCard name={contact.name} job={contact.job} mail={contact.mail} phone={contact.job} />
-        })}
-      </div>
-    )
-  }
 
   return (
     <div className={clsx('mt-13')}>
@@ -51,7 +47,6 @@ export const ContactsContainer = () => {
           Ajouter un contact
         </ButtonSecondary>
       </div>
-      {displayFavoriteContacts()}
     </div>
   )
 }
