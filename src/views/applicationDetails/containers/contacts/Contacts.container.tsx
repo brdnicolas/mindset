0a4dc4f97eaabad12037414db1a42184173bfd93
QStieven <qstievenard@gmail.com>
2024-05-22 13:32:04 +0200

    feat(application/contact): add display of favorites contacts

--- src/views/applicationDetails/containers/contacts/Contacts.container.tsx
+++ src/views/applicationDetails/containers/contacts/Contacts.container.tsx
@@ -6,17 +6,43 @@ import { ContactModal } from './components/modals/contactModal'
 
 export const ContactsContainer = () => {
   const [showContactModal, setShowContactModal] = useState(false)
-  const contactInfo = {
-    name: 'Nicolas Brouard',
-    job: 'Head of RH',
-    mail: 'brdnicolas.contact@gmail.com',
-    phone: '0767939258'
+  const contacts = [
+    {
+      name: 'Nicolas Brouard',
+      job: 'Head of RH',
+      mail: 'brdnicolas.contact@gmail.com',
+      phone: '0767939258',
+      favorite: false
+    },
+    {
+      name: 'Nicolas Brouard',
+      job: 'Head of RH',
+      mail: 'brdnicolas.contact@gmail.com',
+      phone: '0767939258',
+      favorite: true
+    }
+  ]
+
+  const displayFavoriteContacts = () => {
+    const favoriteContacts = contacts.filter((contact) => contact.favorite === true)
+
+    if (favoriteContacts.length === 0) {
+      return
+    }
+
+    return (
+      <div className="flex flex-col">
+        <h2 className={clsx('text-4 text-gray-50 font-bold')}>Favoris</h2>
+        {favoriteContacts.map((contact) => {
+          return <ContactCard name={contact.name} job={contact.job} mail={contact.mail} phone={contact.job} />
+        })}
+      </div>
+    )
   }
 
   return (
     <div className={clsx('mt-13')}>
-      <div className={clsx('flex items-center justify-between')}>
-        <h2 className={clsx('text-4 text-gray-50 font-bold')}>Favoris</h2>
+      <div className="flex justify-end">
         <ButtonSecondary
           onClick={() => {
             setShowContactModal(true)
@@ -25,18 +51,7 @@ export const ContactsContainer = () => {
           Ajouter un contact
         </ButtonSecondary>
       </div>
-      <ContactCard name={contactInfo.name} job={contactInfo.job} mail={contactInfo.mail} phone={contactInfo.job} />
-      {showContactModal && (
-        <ContactModal
-          action="add"
-          name={contactInfo.name}
-          mail={contactInfo.mail}
-          show={showContactModal}
-          onClose={() => {
-            setShowContactModal(false)
-          }}
-        />
-      )}
+      {displayFavoriteContacts()}
     </div>
   )
 }
